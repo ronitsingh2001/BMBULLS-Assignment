fe61f3bff16afca261bf88693e0eb607f9b055fd
Ronit Singh <ronit2001krish@gmail.com>
2025-07-05 17:23:18 +0530

    naming

--- src/components/AnimatedCard.tsx
+++ src/components/AnimatedCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AnimatedCard({ children }: { children: React.ReactNode }) {
   return (
-    <div className="box b">
+    <div className="box relay">
       <div style={{ zIndex: 1 }}>{children}</div>
     </div>
   );
